81062b18664e8a920b125c1f0a628b35b658b8aa
M. Nindra Zaka <mnindrazaka@gmail.com>
2022-08-18 07:12:35 +0700

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-const form = document.getElementById("form");
-const formInput = document.getElementById("form__input");
-const formMessage = document.getElementById("form__message");
-const formResult = document.getElementById("form__result");
+const App = {
+  state: {
+    items: [],
+    inputValue: "",
+  },
 
-const onInputValueChange = () => {
-  if (inputValue === "") {
-    setItems([]);
-  } else {
-    fetch(`https://api.github.com/search/repositories?q=${inputValue}`)
-      .then((res) => res.json())
-      .then((res) => setItems(res.items));
-  }
+  didUpdate(prevState, nextState) {
+    if (prevState.inputValue !== nextState.inputValue) {
+      if (nextState.inputValue === "") {
+        this.setState("items", []);
+      } else {
+        fetch(
+          `https://api.github.com/search/repositories?q=${this.state.inputValue}`
+        )
+          .then((res) => res.json())
+          .then((res) => this.setState("items", res.items));
+      }
+    }
+  },
+
+  setState(key, value) {
+    const prevState = { ...this.state };
+
+    this.state[key] = value;
+    this.render();
+
+    const nextState = this.state;
+
+    this.didUpdate(prevState, nextState);
+  },
+
+  render() {
+    const root = document.getElementById("root");
+    root.innerHTML = `
+    <form id="form">
+      <label>
+        Enter Repo Name :
+        <input type="text" id="form__input" value="${this.state.inputValue}" />
+      </label>
+      <p>${this.state.inputValue}</p>
+
+      <div>
+        ${this.state.items.map((item) => `<p>${item.full_name}</p>`).join("")}
+      </div>
+    </form>
+    `;
+
+    const formInput = document.getElementById("form__input");
+    formInput.addEventListener("input", (event) => {
+      this.setState("inputValue", event.target.value);
+    });
+  },
 };
 
-const render = () => {
-  formInput.value = inputValue;
-  formMessage.innerText = inputValue;
-  formResult.innerHTML = items
-    .map((item) => `<p>${item.full_name}</p>`)
-    .join("");
-};
-
-let items = [];
-const setItems = (newItems) => {
-  items = newItems;
-  render();
-};
-
-let inputValue = "";
-const setInputValue = (newInputvalue) => {
-  inputValue = newInputvalue;
-  render();
-  onInputValueChange();
-};
-
-formInput.addEventListener("input", (event) => {
-  setInputValue(event.target.value);
-});
+App.render();
