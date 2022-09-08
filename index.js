ca8e8bf40b81c331d09b56ab06e7ff2e5e525869
M. Nindra Zaka <mnindrazaka@gmail.com>
2022-09-08 07:35:12 +0700

    handle focus element

--- index.js
+++ index.js
@@ -1,55 +1,127 @@
-const App = {
+const Root = {
   state: {
     items: [],
     inputValue: "",
+    error: null,
+    loading: false,
   },
 
   didUpdate(prevState, nextState) {
     if (prevState.inputValue !== nextState.inputValue) {
       if (nextState.inputValue === "") {
-        this.setState("items", []);
+        this.setState({ items: [] });
       } else {
+        this.setState({ loading: true });
         fetch(
           `https://api.github.com/search/repositories?q=${this.state.inputValue}`
         )
           .then((res) => res.json())
-          .then((res) => this.setState("items", res.items));
+          .then((res) => this.setState({ items: res.items, error: null }))
+          .catch(() =>
+            this.setState({ items: [], error: "Something went wrong" })
+          )
+          .finally(() => this.setState({ loading: false }));
       }
     }
   },
 
-  setState(key, value) {
+  setState(newState) {
     const prevState = { ...this.state };
+    const nextState = { ...prevState, ...newState };
+    this.state = nextState;
 
-    this.state[key] = value;
-    this.render();
-
-    const nextState = this.state;
+    this.render(App);
 
     this.didUpdate(prevState, nextState);
   },
 
-  render() {
+  render(App) {
     const root = document.getElementById("root");
-    root.innerHTML = `
-    <form id="form">
-      <label>
-        Enter Repo Name :
-        <input type="text" id="form__input" value="${this.state.inputValue}" />
-      </label>
-      <p>${this.state.inputValue}</p>
+    const app = App();
 
-      <div>
-        ${this.state.items.map((item) => `<p>${item.full_name}</p>`).join("")}
-      </div>
-    </form>
-    `;
+    const focusedElementId = document.activeElement.id;
+    const focusedElementSelectionStart = document.activeElement.selectionStart;
+    const focusedElementSelectionEnd = document.activeElement.selectionEnd;
 
-    const formInput = document.getElementById("form__input");
-    formInput.addEventListener("input", (event) => {
-      this.setState("inputValue", event.target.value);
-    });
+    root.innerHTML = "";
+    root.append(app);
+
+    if (focusedElementId) {
+      const focusedElement = document.getElementById(focusedElementId);
+      focusedElement.focus();
+      focusedElement.selectionStart = focusedElementSelectionStart;
+      focusedElement.selectionEnd = focusedElementSelectionEnd;
+    }
   },
 };
 
-App.render();
+function Input() {
+  const handleInput = (event) => {
+    Root.setState({ inputValue: event.target.value });
+  };
+
+  const inputElement = document.createElement("input");
+  inputElement.id = "input";
+  inputElement.type = "text";
+  inputElement.value = Root.state.inputValue;
+  inputElement.oninput = handleInput;
+
+  const labelElement = document.createElement("label");
+  labelElement.append("Enter Repo Name :");
+  labelElement.append(inputElement);
+
+  console.log(Root.state.cursorPosition);
+
+  return labelElement;
+}
+
+function TextPreview() {
+  const paragraphElement = document.createElement("p");
+  paragraphElement.textContent = Root.state.inputValue;
+  return paragraphElement;
+}
+
+function Items() {
+  const children = Root.state.items.map((item) => {
+    const paragraphElement = document.createElement("p");
+    paragraphElement.textContent = item.full_name;
+    return paragraphElement;
+  });
+
+  const container = document.createElement("div");
+  container.append(...children);
+  return container;
+}
+
+function Loading() {
+  const paragraphElement = document.createElement("p");
+  paragraphElement.textContent = "Loading...";
+  return paragraphElement;
+}
+
+function EmptyItems() {
+  const paragraphElement = document.createElement("p");
+  paragraphElement.textContent = "You don't have any items";
+  return paragraphElement;
+}
+
+function App() {
+  const input = Input();
+  const text = TextPreview();
+  const items = Items();
+  const emptyItems = EmptyItems();
+  const loading = Loading();
+  const container = document.createElement("div");
+  container.append(input);
+  container.append(text);
+  container.append(
+    Root.state.loading
+      ? loading
+      : Root.state.items.length > 0
+      ? items
+      : emptyItems
+  );
+  return container;
+}
+
+Root.render(App);
